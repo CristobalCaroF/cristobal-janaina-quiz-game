d426ba5ebcb72e784c85d1ac2a8f1ce2f56b25fa
Cristobal Caro <cacaro@uc.cl>
2024-06-07 15:08:38 +0200

    fetching question data and showing it in the main page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,26 @@
-import Image from "next/image";
-// import styles from "./page.module.css";
+// import Image from "next/image";
+// import styled from "styled-components";
+// import Card from "../components/Card.js";
+import useSWR from "swr";
+import Link from "next/link.js";
+
+const fetcher = (...args) => fetch(args).then((res) => res.json());
 
 export default function Home() {
-  return <main>Welcome to the best quiz game ever</main>;
+  const { data } = useSWR("/api/questions", fetcher);
+
+  if (!data) return null;
+  return (
+    <>
+      <ul>
+        {data.map((question) => {
+          return (
+            <li key={question._id}>
+              {question.question} {question.correct}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
