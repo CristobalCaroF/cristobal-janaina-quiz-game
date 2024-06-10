9c06d8a7363bf6ba5d0eea9978ab9de94fa69879
Janaina Voigt <jv.de@icloud.com>
2024-06-10 21:50:43 +0200

    Added the title for the new register and api for the login

---
+++ pages/api/login/index.js
@@ -0,0 +1,18 @@
+import dbConnect from "@/db/dbConnect";
+import User from "@/db/models/User";
+
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "POST") {
+    const { username, password } = request.body;
+    const { db } = User;
+    const user = await db.collection("users").findOne({ username });
+
+    if (user && user.password === password) {
+      response.status(200);
+    } else {
+      response.status(401);
+    }
+  }
+}
