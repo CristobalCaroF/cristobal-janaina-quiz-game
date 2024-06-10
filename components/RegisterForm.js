bb425ae58c7520c98ec5259d520e9f7acd679288
Janaina Voigt <jv.de@icloud.com>
2024-06-10 19:25:14 +0200

    Added the link to login page

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -1,5 +1,6 @@
 import useSWR from "swr";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 // const fetcher = (...args) => fetch(args).then((res) => res.json());
 
@@ -34,32 +35,46 @@ export default function RegisterForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="user-input">Enter your Name:</label>
-      <input type="text" id="user-input" name="username" required />
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="user-input">Enter your Name:</label>
+        <input
+          type="text"
+          id="user-input"
+          name="username"
+          placeholder="Name"
+          required
+        />
 
-      <label htmlFor="password">Password:</label>
-      <input
-        type="password"
-        id="password"
-        name="password"
-        // value={password}
-        // onChange={handleChange}
-        // pattern="(?=.*\d)(?=.*[a-zA-Z]).{8,}"
-        // title="Must contain at least one letter, one number, and be at least 8 characters long"
-        required
-      />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          // value={password}
+          // onChange={handleChange}
+          // pattern="(?=.*\d)(?=.*[a-zA-Z]).{8,}"
+          // title="Must contain at least one letter, one number, and be at least 8 characters long"
+          placeholder="Password"
+          required
+        />
 
-      <label htmlFor="email">Email:</label>
-      <input
-        type="email"
-        id="email"
-        name="email"
-        // value={email}
-        // onChange={handleChange}
-        required
-      />
-      <button type="submit">Submit</button>
-    </form>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          // value={email}
+          // onChange={handleChange}
+          required
+        />
+        <button type="submit">Submit</button>
+      </form>
+
+      <div>
+        <Link href="/"> Já tenho cadastro</Link>
+      </div>
+    </>
   );
 }
