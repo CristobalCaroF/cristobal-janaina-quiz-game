20726cacea29e90e282e80df7a340d05a4e89f7d
Janaina Voigt <jv.de@icloud.com>
2024-06-11 15:55:37 +0200

    Deleted the pages login and registration because login will be done with github authentication

---
+++ styles.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  *, 
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+ 
+
+  body {
+    margin: 0;
+    font-family: system-ui;
+    padding: 2rem;
+  }
+`;
+
+// :root {
+//     --primary-color: #22babb;
+//     --secondary-color: #f24405;
+//     --primary-background: #d3d3d3;
+//   }
