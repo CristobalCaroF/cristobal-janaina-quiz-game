7bf20d946400bd6d2e2925864a955de725d74d33
Janaina Voigt <jv.de@icloud.com>
2024-06-11 15:00:42 +0200

    Added the pages to nextAuth with github

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -17,8 +17,16 @@ export const authOptions = {
   debug: true,
 
   callbacks: {
-    async session({ session, user }) {
-      session.user.userId = user.id;
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken;
       return session;
     },
   },
