469d9cc19d186b0d8ca36b7a6c2855ce0746b40d
Janaina Voigt <jv.de@icloud.com>
2024-06-12 15:01:08 +0200

    Created the home-page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Profile from "@/components/Profile";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <Profile />
+      <Link href="/pages/homepage.js">Aqui</Link>
     </>
   );
 }
