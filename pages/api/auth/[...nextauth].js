1e23f829f2684e83d432ffc42bc7f79b4ceced07
Janaina Voigt <jv.de@icloud.com>
2024-06-12 20:59:08 +0200

    created de home-page

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,6 @@ import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/db/mongodb";
 
 export const authOptions = {
-  adapter: MongoDBAdapter(clientPromise),
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
@@ -15,21 +13,6 @@ export const authOptions = {
   ],
 
   debug: true,
-
-  callbacks: {
-    async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token, user }) {
-      // Send properties to the client, like an access_token from a provider.
-      session.accessToken = token.accessToken;
-      return session;
-    },
-  },
 };
 
 export default NextAuth(authOptions);
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,34 @@
-import Profile from "@/components/Profile";
-import Link from "next/link";
+import Nav from "@/components/Nav";
+import ProfileInfo from "@/components/ProfileInfo";
+import SignIn from "@/components/SignIn";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 
 export default function Home() {
+  const { data: session } = useSession();
+
+  if (!session) {
+    return <SignIn />;
+  }
+
   return (
     <>
-      <Profile />
-      <Link href="/pages/homepage.js">Aqui</Link>
+      <ProfileInfo session={session} />
+      <div>
+        <Nav />
+      </div>
+      <div>
+        <Image
+          src="/Friends1.png"
+          alt="friends-foto"
+          width={200}
+          height={100}
+        />
+      </div>
+
+      <div>
+        <button type="button ">PLAY</button>
+      </div>
     </>
   );
 }
